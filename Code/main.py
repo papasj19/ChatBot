0ace124f8e707b39eb2cc4fa7f6360099807c8ae
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-02-27 12:05:03 +0100

    adding the functions from the NLP masterclass

--- Code/main.py
+++ Code/main.py
@@ -14,6 +14,51 @@ import sklearn.manifold
 import scipy
 
 
+def tokenize(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    return tokens
+
+
+def stem(sentence):
+    words = tokenize(sentence)
+    sentence_stemmed = []
+    porter = PorterStemmer()
+    for word in words:
+        sentence_stemmed.append(word.format(word, porter.stem(word)))
+    return sentence_stemmed
+
+
+def lemmatize(sentence):
+    lemmatized = []
+    lemmatizer = WordNetLemmatizer()
+    words = tokenize(sentence)
+    for word in words:
+        lemmatized.append(word.format(word, lemmatizer.lemmatize(word, pos="v")))
+    return lemmatized
+
+
+def filter_stop_words(sentence):
+    words = tokenize(sentence)
+    stop_words = stopwords.words('english')
+    filtered = []
+    for word in words:
+        if word not in stop_words:
+            filtered.append(word)
+    return filtered
+
+
+def fit_vocabulary(data):
+    vocabulary = {}
+
+    temp = []
+    iter = 0
+
+    for sentence in data:
+        temp[iter] = data.split(" ")
+        iter += 1
+        dudes = stem(sentence)
+    return vocabulary
+
+
 if __name__ == '__main__':
     print('New Project')
-
