771c97cbc1dd471ecca2d057673e90c3a73909cc
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-02-27 12:02:00 +0100

    adding the libraries for NLP from the masterclass

--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,19 @@
+import nltk
+from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer, SnowballStemmer
+from nltk.corpus import stopwords
+
+import pandas as pd
+
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+
+import sklearn
+from sklearn import datasets, metrics, feature_extraction
+from sklearn.feature_extraction.text import CountVectorizer
+import sklearn.manifold
+import scipy
+
+
 if __name__ == '__main__':
     print('New Project')
 
